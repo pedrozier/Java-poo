1e6d3b9b3b8de5aa9fdfe56e77f47a201254614e
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2021-07-24 12:47:07 -0300

    Add files via upload

---
+++ Aula02/src/aula02/Aula02.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02;
+
+/**
+ *
+ * @author zier
+ */
+public class Aula02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "'marca1'";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.rabiscar();
+        c2.status();
+
+    }
+    
+}
